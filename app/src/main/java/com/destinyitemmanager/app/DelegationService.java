9ec25e0fc08e677c13c7d133679f02b96ff9db2e
Kyle Shay <kshay@dropbox.com>
2023-04-03 19:16:07 -0700

    new pwa

---
+++ app/src/main/java/com/destinyitemmanager/app/DelegationService.java
@@ -0,0 +1,14 @@
+package com.destinyitemmanager.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
